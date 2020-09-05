3eebb77a104779867a8d9b9cba970952f6150b24
Martin <martin.ansteensen@outlook.com>
2020-09-05 09:46:54 +0200

    first take at odometry

--- improvedRobotCar/encoder.ino
+++ improvedRobotCar/encoder.ino
@@ -1,29 +1,73 @@
-#include "TimerOne.h"
+#define inputCLK 48
+#define inputDT 49
 
-double diskslots = 24;
+// 40 ticks per revolution
 
-const byte frontLeftPin = 18;
-unsigned int frontLeftCounter = 0;
-
-void ISRcountFrontLeft(){
-  frontLeftCounter++; 
-}
-
-void ISRtimerone(){
-  Timer1.detachInterrupt(); // Stop the timer
-  //  Serial.print("Motor Speed front left: ");
-  float frontLeftRotation = (frontLeftCounter/diskslots)*60.00;
-  //Serial.print(24.00/frontLeftCounter*119.4); Serial.print("mm, "); //24/(pi*38)
-  Serial.print(frontLeftCounter*0.201); Serial.print("mm, "); //24/(pi*38)
-  Serial.println("");
-  frontLeftCounter = 0;
-  Timer1.attachInterrupt(ISRtimerone);
-}
+int counter = 0;
+int currentStateCLK;
+int previousStateCLK;
+String encdir ="";
 
 void encoderSetup(){
- Serial.println("Encoder setup");
- Timer1.initialize(1000000);
- attachInterrupt(digitalPinToInterrupt(frontLeftPin), ISRcountFrontLeft, RISING);
- Timer1.attachInterrupt(ISRtimerone);
+  Serial.println("Encoder initializing");
+  // set the encoder pins as inputs
+  pinMode (inputCLK,INPUT);
+  pinMode (inputDT,INPUT);
 
+  // Read the initial state of inputCLK
+  // Assign to previousStateCLK variable
+  previousStateCLK = digitalRead(inputCLK);
+  Serial.println("Encoder setup finished");
 }
+
+void readEncoders(){
+  // Read the current state of inputCLK
+  currentStateCLK = digitalRead(inputCLK);
+  
+  // If the previous and the current state of the inputCLK are different then a pulse has occured
+  if (currentStateCLK != previousStateCLK){ 
+     
+   // If the inputDT state is different than the inputCLK state then 
+   // the encoder is rotating counterclockwise
+   if (digitalRead(inputDT) != currentStateCLK) { 
+     counter --;
+     encdir ="CCW";
+     
+   } else {
+     // Encoder is rotating clockwise
+     counter ++;
+     encdir ="CW";
+     
+   }
+   Serial.print("Direction: ");
+   Serial.print(encdir);
+   Serial.print(" -- Value: ");
+   Serial.println(counter);
+  } 
+  // Update previousStateCLK with the current state
+  previousStateCLK = currentStateCLK;  
+}
+
+// All angles are in radians
+int distLeftX = 0;
+int distRightX = 0;
+int distBottomY = 0;
+int xToCenterDist = 10;
+int yToCenterDist = 10;
+int angle;
+
+int distMiddle;
+int pastOrientation = 0;
+int newOrientation = 0;
+int pastX = 0;
+int pastY = 0;
+int currentX = 0;
+int currentY = 0;
+
+
+currentX = pastX + ((distMiddle/angle)*sin(angle)/cos(angle))*cos(pastOrientation + angle/2);
+currentY = pastX + ((distMiddle/angle)*sin(angle)/cos(angle))*cos(pastOrientation + angle/2);
+newOrientation = pastOrientation + angle;
+pastX = currentX;
+pastY = currentY; 
+
